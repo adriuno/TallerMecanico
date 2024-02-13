4377ee1aada0bdac18e593c90f245ad46bdd06e9
JMMendi <mendizabaly@gmail.com>
2024-02-13 12:17:52 +0100

    La clase Vehiculos pasa los test.

---
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/Vehiculos.java
@@ -0,0 +1,38 @@
+package org.iesalandalus.programacion.tallermecanico.modelo.negocio;
+
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Vehiculo;
+
+import javax.naming.OperationNotSupportedException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Vehiculos {
+    private List<Vehiculo> vehiculos;
+
+    public Vehiculos() {
+        vehiculos = new ArrayList<>();
+    }
+    public List<Vehiculo> get() {
+        return vehiculos;
+    }
+    public void insertar(Vehiculo vehiculo) throws OperationNotSupportedException {
+        Objects.requireNonNull(vehiculo, "No se puede insertar un vehículo nulo.");
+        if (vehiculos.contains(vehiculo)) {
+            throw new OperationNotSupportedException("Ya existe un vehículo con esa matrícula.");
+        }
+        vehiculos.add(vehiculo);
+    }
+    public Vehiculo buscar(Vehiculo vehiculo) {
+        Objects.requireNonNull(vehiculo, "No se puede buscar un vehículo nulo.");
+        int indice = vehiculos.indexOf(vehiculo);
+        return indice == -1 ? null : vehiculos.get(indice);
+    }
+    public void borrar(Vehiculo vehiculo) throws OperationNotSupportedException {
+        Objects.requireNonNull(vehiculo, "No se puede borrar un vehículo nulo.");
+        if (!vehiculos.contains(vehiculo)) {
+            throw new OperationNotSupportedException("No existe ningún vehículo con esa matrícula.");
+        }
+        vehiculos.remove(vehiculo);
+    }
+}
